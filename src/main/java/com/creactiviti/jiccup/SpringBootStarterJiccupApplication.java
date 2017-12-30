378a5ecd6d7b926af5b5fad4a54daecce7655e05
Cohen, Arik <arik.cohen@smithsonianchannel.com>
2017-12-30 13:06:29 -0500

    first commit

---
+++ src/main/java/com/creactiviti/jiccup/SpringBootStarterJiccupApplication.java
@@ -0,0 +1,12 @@
+package com.creactiviti.jiccup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterJiccupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStarterJiccupApplication.class, args);
+	}
+}
